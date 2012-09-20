c781ac7a2277d292390388bd76cb28eb4397b290
Jalal Ahmad <jalal.ahmad@gmail.com>
2012-09-21 01:47:15 +0530

    Temporary
    Trying out branch

---
+++ HyperCache/CacheManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyperCache
+{
+    public class CacheManager
+    {
+        protected ICache cache;
+        protected WorkQueue queue;
+
+        public CacheManager(ICache cache, int maxConcurrent = 10)
+        {
+            if (cache != null)
+                this.cache = cache;
+            else
+                cache = new ObjectCacheProvider();
+            queue = new WorkQueue(cache, maxConcurrent);
+        }
+
+        public TResult Process<TResult>(string id, Func<TResult> func, DateTimeOffset absoluteExpiration)
+        {
+            if (cache[id] != null)
+                return (TResult)cache[id];
+            else
+            {
+                queue.Enqueue<TResult>(id, func, absoluteExpiration);
+                return default(TResult);
+            }
+
+        }
+    }
+}
---
+++ HyperCache/ICache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyperCache
+{
+    public interface ICache
+    {
+        object this[string index] { get; }
+        void Set(string id, object value, DateTimeOffset absoluteExpiration);
+    }
+}
