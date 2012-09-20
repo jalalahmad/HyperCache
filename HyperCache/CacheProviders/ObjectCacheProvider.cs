50a4ecb5512e6c16f5c8fa6376a5a564ecb6be80
Jalal Ahmad <jalal.ahmad@gmail.com>
2012-09-21 03:22:24 +0530

    Object Cache and Work Queue

---
+++ HyperCache/CacheProviders/ObjectCacheProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+
+namespace HyperCache.CacheProviders
+{
+    class ObjectCacheProvider : ICache
+    {
+        protected MemoryCache cache = MemoryCache.Default;
+        #region ICache Members
+
+        public object this[string index]
+        {
+            get { return cache[index]; }
+        }
+
+        public void Set(string id, object value, DateTimeOffset absoluteExpiration)
+        {
+            cache.Set(new CacheItem(id, value), new CacheItemPolicy { AbsoluteExpiration = absoluteExpiration });
+        }
+
+        #endregion
+
+
+    }
+
+}
---
+++ HyperCache/WorkQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperCache
+{
+    public class WorkQueue
+    {
+        public int MaxConcurrency { get; protected set; }
+        private LimitedConcurrencyLevelTaskScheduler lcts;
+        protected TaskFactory factory;
+        protected ICache Cache { get; set; }
+
+
+
+        public WorkQueue(ICache cache, int maxConcurrency = 10)
+        {
+            this.MaxConcurrency = maxConcurrency;
+            this.Cache = cache;
+            lcts = new LimitedConcurrencyLevelTaskScheduler(this.MaxConcurrency);
+            factory = new TaskFactory(lcts);
+        }
+
+        public void Enqueue<TResult>(string id, Func<TResult> func, DateTimeOffset absoluteExpiration)
+        {
+            var task = factory.StartNew(func);
+            task.ContinueWith((antecedent) =>
+            {
+                this.Cache.Set(id, antecedent.Result, absoluteExpiration);
+            });
+        }
+    }
+}
