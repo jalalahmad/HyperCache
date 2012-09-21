ed50540a267059da52cf99c5787742d905f3dd2c
Jalal Ahmad <jalal.ahmad@gmail.com>
2012-09-21 10:40:37 +0530

    Primary Sql support

---
+++ HyperCache/Sql/SqlCacheManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace HyperCache.Sql
+{
+    public class SqlCacheManager
+    {
+        protected CacheManager cacheManager;
+        public string ConnectionString { get; protected set; }
+        public SqlCacheManager(string connectionString)
+        {
+            cacheManager = new CacheManager(null);
+        }
+
+        public string Process(string query, DateTimeOffset absoluteExpiration)
+        {
+            return cacheManager.Process<string>(query, () =>
+            {
+                var val = string.Empty;
+                using (var sqlConnection = new SqlConnection())
+                {
+                    sqlConnection.Open();
+                    using (var sqlCommand = new SqlCommand(query + " for xml"))
+                    {
+                        var reader = sqlCommand.ExecuteXmlReader();
+                        if (reader.Read())
+                        {
+                            while (reader.ReadState != System.Xml.ReadState.EndOfFile)
+                            {
+                                val = reader.ReadOuterXml();
+                            }
+                        }
+                    }
+                }
+                return val;
+            }, absoluteExpiration);
+        }
+
+    }
+}
